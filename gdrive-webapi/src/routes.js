9d256c4ba83daa77ba50def036f1cade44f7c30f
tiago.bani <tiago.bani@yahoo.com.br>
2021-09-18 22:08:07 -0300

    feat[gdrive-webapi]: add route post

--- gdrive-webapi/src/routes.js
+++ gdrive-webapi/src/routes.js
@@ -1,16 +1,19 @@
+import { dirname, resolve } from 'path'
+import { fileURLToPath, URL } from 'url'
+import { pipeline } from 'stream/promises'
+
 import FileHelper from './fileHelper.js'
 import { logger } from './logger.js'
-import { dirname, resolve } from 'path'
-import { fileURLToPath } from 'url'
+import UploadHandler from './uploadHandler.js'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 const defaultDownloadsFolder = resolve(__dirname, '../', 'downloads')
 
 export default class Routes {
-	io
 	constructor(downloadsFolder = defaultDownloadsFolder) {
 		this.downloadsFolder = downloadsFolder
 		this.fileHelper = FileHelper
+		this.io = {}
 	}
 
 	setSocketInstance(io) {
@@ -27,11 +30,34 @@ export default class Routes {
 	}
 
 	async post(request, response) {
-		logger.info('post')
-		response.end()
+		const { headers } = request
+		const { searchParams } = new URL(request.url)
+		const socketId = searchParams.get('socketId')
+
+		const uploadHandler = new UploadHandler({
+			socketId,
+			io: this.io,
+			downloadsFolder: this.downloadsFolder,
+		})
+
+		const onFinish = (res) => () => {
+			res.writeHead(200)
+			const data = JSON.stringify({
+				result: 'Files uploaded with success',
+			})
+			res.end(data)
+		}
+
+		const busboyInstance = uploadHandler.registerEvents(
+			headers,
+			onFinish(response)
+		)
+
+		await pipeline(request, busboyInstance)
+
+		logger.info('Request finished with success!')
 	}
 	async get(request, response) {
-		logger.info('get')
 		const files = await this.fileHelper.getFileStatus(this.downloadsFolder)
 
 		response.writeHead(200)
